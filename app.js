4a7aa4f2cc62f751e6c7602a4f0c614a12daf497
markdw95 <mark.wilson2014@gmail.com>
2023-03-12 18:29:03 -0400

    splash page

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(userRouter);
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/test', (req, res) => {
   res.send('Hello world');
